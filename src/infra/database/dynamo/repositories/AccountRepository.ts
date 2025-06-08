4af968380caf1260a09466519ddbd1f8ff2ddcf5
IanFsciety <iandelas123@outlook.com>
2025-06-08 18:22:40 -0400

    create account repository

---
+++ src/infra/database/dynamo/repositories/AccountRepository.ts
@@ -0,0 +1,21 @@
+import { Account } from "@application/entities/Account";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { dynamoClient } from "@infra/clients/dyamoClient";
+import { Injectable } from "@kernel/decorators/Injectable";
+import { AppConfig } from "@shared/config/AppConfig";
+import { AccountItem } from "../items/AccountItem";
+
+@Injectable()
+export class AccountRepository {
+  constructor(private readonly config: AppConfig) {}
+  async create(account: Account): Promise<void> {
+    const accountItem = AccountItem.fromEntity(account)
+
+    const command = new PutCommand({
+      TableName: this.config.db.dynamodb.mainTable,
+      Item: accountItem.toItem(),
+    });
+
+    await dynamoClient.send(command);
+  }
+}
