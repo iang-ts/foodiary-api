2fa05d1b622b701396f235365719329746790689
IanFsciety <iandelas123@outlook.com>
2025-06-08 18:23:11 -0400

    persist account item on dynamo database

--- src/application/usecases/auth/SignUpUseCase.ts
+++ src/application/usecases/auth/SignUpUseCase.ts
@@ -1,20 +1,28 @@
+import { Account } from "@application/entities/Account";
+import { AccountRepository } from "@infra/database/dynamo/repositories/AccountRepository";
 import { Injectable } from "@kernel/decorators/Injectable";
 import { AuthGateway } from "src/infra/gateways/AuthGateway";
 
 @Injectable()
 export class SignUpUseCase {
-  constructor(private readonly authGateway: AuthGateway) { }
+  constructor(
+    private readonly authGateway: AuthGateway,
+    private readonly accountRepository: AccountRepository,
+  ) { }
 
   async execute({
     email,
     password
   }: SignUpUseCase.Input): Promise<SignUpUseCase.Output> {
-    await this.authGateway.signUp({ email, password })
+    const { externalId } = await this.authGateway.signUp({ email, password });
+
+    const account = new Account({ email, externalId })
+    await this.accountRepository.create(account)
 
     const {
       accessToken,
       refreshToken
-    } = await this.authGateway.signIn({ email, password })
+    } = await this.authGateway.signIn({ email, password });
 
     return {
       accessToken,
