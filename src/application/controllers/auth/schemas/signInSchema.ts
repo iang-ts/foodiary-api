c926ae6c4b971803b21c713968032700dda6f77b
IanFsciety <iandelas123@outlook.com>
2025-06-08 21:50:38 -0400

    create sign in f

---
+++ src/application/controllers/auth/schemas/signInSchema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const signInSchema = z.object({
+    email: z.string().min(1, "Email is required").email("Invalid email format"),
+    password: z.string().min(6, "Password should be at least 6 characters long."),
+})
+
+
+export type SignInBody = z.infer<typeof signInSchema>;
