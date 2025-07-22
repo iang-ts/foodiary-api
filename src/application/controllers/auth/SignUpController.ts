8f1875094be1baa7873a1c521085dbc675962209
IanFsciety <iandelas123@outlook.com>
2025-07-21 20:50:32 -0400

    feat: take profile from singup controller body

--- src/application/controllers/auth/SignUpController.ts
+++ src/application/controllers/auth/SignUpController.ts
@@ -13,9 +13,12 @@ export class SignUpController extends Controller<'public', SignUpController.Resp
   }
 
   protected override async handle({ body }: Controller.Request<'public', SignUpBody>): Promise<Controller.Response<SignUpController.Response>> {
-    const { account } = body
+    const { account, profile } = body
 
-    const { accessToken, refreshToken } = await this.signUpUseCase.execute(account);
+    const { accessToken, refreshToken } = await this.signUpUseCase.execute({
+      account,
+      profile
+    });
     return {
       statusCode: 201,
       body: {
