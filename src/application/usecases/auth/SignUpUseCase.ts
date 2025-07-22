9147d9439670f4e5dc588a60a824d74fe400a376
IanFsciety <iandelas123@outlook.com>
2025-07-21 20:52:45 -0400

    feat: use saga at signup use case

--- src/application/usecases/auth/SignUpUseCase.ts
+++ src/application/usecases/auth/SignUpUseCase.ts
@@ -1,7 +1,11 @@
 import { Account } from "@application/entities/Account";
+import { Goal } from "@application/entities/Goal";
+import { Profile } from "@application/entities/Profile";
 import { EmailAlreadyInUse } from "@application/errors/application/EmailAlreadyInUse";
 import { AccountRepository } from "@infra/database/dynamo/repositories/AccountRepository";
+import { SignUpUnitOfWork } from "@infra/database/dynamo/uow/SignUpUnitOfWork";
 import { Injectable } from "@kernel/decorators/Injectable";
+import { Saga } from "@shared/saga/Saga";
 import { AuthGateway } from "src/infra/gateways/AuthGateway";
 
 @Injectable()
@@ -9,45 +13,78 @@ export class SignUpUseCase {
   constructor(
     private readonly authGateway: AuthGateway,
     private readonly accountRepository: AccountRepository,
+    private readonly signUpUow: SignUpUnitOfWork,
+    private readonly saga: Saga,
   ) { }
 
   async execute({
-    email,
-    password
-  }: SignUpUseCase.Input): Promise<SignUpUseCase.Output> {
-
-    const emailAlreadyInUse = await this.accountRepository.findByEmail(email);
-
-    if (emailAlreadyInUse) throw new EmailAlreadyInUse();
-
-    const account = new Account({ email })
-
-    const { externalId } = await this.authGateway.signUp({
+    account: {
       email,
       password,
-      internalId: account.id
+    },
+    profile: profileInfo,
+  }: SignUpUseCase.Input): Promise<SignUpUseCase.Output> {
+
+    return this.saga.run(async () => {
+      const emailAlreadyInUse = await this.accountRepository.findByEmail(email);
+
+      if (emailAlreadyInUse) throw new EmailAlreadyInUse();
+
+      const account = new Account({ email })
+      const profile = new Profile({
+        ...profileInfo,
+        accountId: account.id
+      });
+      const goal = new Goal({
+        accountId: account.id,
+        calories: 2000,
+        carbohydrates: 200,
+        fats: 50,
+        proteins: 100
+      });
+
+      const { externalId } = await this.authGateway.signUp({
+        email,
+        password,
+        internalId: account.id
+      });
+      this.saga.addCompensation(() => this.authGateway.deleteUser({ externalId }));
+
+      account.externalId = externalId;
+
+      await this.signUpUow.run({
+        account,
+        goal,
+        profile
+      });
+
+      const {
+        accessToken,
+        refreshToken
+      } = await this.authGateway.signIn({ email, password });
+
+      return {
+        accessToken,
+        refreshToken
+      }
     });
-
-    account.externalId = externalId;
-
-    await this.accountRepository.create(account)
-
-    const {
-      accessToken,
-      refreshToken
-    } = await this.authGateway.signIn({ email, password });
-
-    return {
-      accessToken,
-      refreshToken
-    }
   };
 }
 
 export namespace SignUpUseCase {
   export type Input = {
-    email: string;
-    password: string;
+    account: {
+      email: string;
+      password: string;
+    };
+    profile: {
+      name: string;
+      birthDate: Date;
+      gender: Profile.Gender;
+      height: number;
+      weight: number;
+      activityLevel: Profile.ActivityLevel;
+    }
   };
 
   export type Output = {
