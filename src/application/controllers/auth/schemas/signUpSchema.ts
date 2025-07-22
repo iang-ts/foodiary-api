5d10f909edfbb9dc12b120239b6bf45b924d51af
IanFsciety <iandelas123@outlook.com>
2025-07-21 20:49:50 -0400

    feat: add profile at signup schema

--- src/application/controllers/auth/schemas/signUpSchema.ts
+++ src/application/controllers/auth/schemas/signUpSchema.ts
@@ -1,10 +1,22 @@
+import { Profile } from "@application/entities/Profile";
 import { z } from "zod";
 
 export const signUpSchema = z.object({
   account: z.object({
     email: z.string().min(1, "email is required").email("Invalid email format"),
     password: z.string().min(6, "password should be at least 6 characters long."),
-  })
+  }),
+  profile: z.object({
+    name: z.string().min(1, "name is required"),
+    birthDate: z.string()
+      .min(1, "birthDate is required")
+      .date("'birthDate' must be a valid date (YYYY-MM-DD)")
+      .transform((date) => new Date(date)),
+    gender: z.nativeEnum(Profile.Gender),
+    height: z.number().min(1, "height is required"),
+    weight: z.number().min(1, "weight is required"),
+    activityLevel: z.nativeEnum(Profile.ActivityLevel),
+  }),
 })
 
 
