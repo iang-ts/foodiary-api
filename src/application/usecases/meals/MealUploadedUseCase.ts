19ac07a7d582faee726fc7f150590df28f9d156d
IanFsciety <iandelas123@outlook.com>
2025-10-26 20:16:33 -0400

    feat: (upload) publish message on queue after upload meal

--- src/application/usecases/meals/MealUploadedUseCase.ts
+++ src/application/usecases/meals/MealUploadedUseCase.ts
@@ -2,13 +2,15 @@ import { Meal } from "@application/entities/Meal";
 import { ResourceNotFound } from "@application/errors/application/ResourceNotFound";
 import { MealRepository } from "@infra/database/dynamo/repositories/MealRepository";
 import { MealsFileStorageGatway } from "@infra/gateways/MealsFileStorageGateway";
+import { MealsQueueGateway } from "@infra/gateways/MealsQueueGateway";
 import { Injectable } from "@kernel/decorators/Injectable";
 
 @Injectable()
 export class MealUploadedUseCase {
    constructor(
       private readonly mealRepository: MealRepository,
-      private readonly mealsFileStorageGatway: MealsFileStorageGatway
+      private readonly mealsFileStorageGatway: MealsFileStorageGatway,
+      private readonly mealsQueueGateway: MealsQueueGateway,
     ) {}
 
     async execute({
@@ -29,7 +31,11 @@ export class MealUploadedUseCase {
 
       meal.status = Meal.Status.QUEUED;
 
-      await this.mealRepository.save(meal)
+      await this.mealRepository.save(meal);
+      await this.mealsQueueGateway.publish({
+        accountId,
+        mealId,
+      });
     }
 }
 
